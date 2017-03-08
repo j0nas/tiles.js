3d916ad9ff1f15b0cf8aed9e47431e144c5b0b11
Jonas Jensen <jonasj@mesan.no>
2017-03-08 23:50:34 +0100

    progress

--- src/client/components/Tile/index.jsx
+++ src/client/components/Tile/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import classNames from 'classnames';
 import './style.scss';
 
-const Tile = ({ children }) =>
-  <div className="tiles-tile">{children}</div>;
+
+const Tile = ({ children, columnCount, centerText, padText }) => {
+  const classes = classNames({
+    'tiles-tile': true,
+    [`tiles-tile-col-${columnCount}`]: true,
+    'tiles-tile-center-text': centerText,
+    'tiles-tile-pad-text': padText,
+  });
+  return <div className={classes}>{children}</div>;
+};
 
 Tile.propTypes = {
   children: React.PropTypes.node.isRequired,
+  columnCount: React.PropTypes.number,
+  centerText: React.PropTypes.bool,
+  padText: React.PropTypes.bool,
+};
+
+Tile.defaultProps = {
+  columnCount: 1,
+  centerText: true,
+  padText: true,
 };
 
 export default Tile;
--- src/client/views/Main/index.jsx
+++ src/client/views/Main/index.jsx
@@ -6,18 +6,36 @@ import './style.scss';
 const MainView = () =>
   <div className="tiles-main-contain">
     <Row>
-      <Tile>13</Tile>
-      <Tile>2324</Tile>
-      <Tile>2433</Tile>
-      <Tile>4423</Tile>
+      <Tile>
+        <div>
+          <div>onsdag</div>
+          <div style={{ fontSize: 44 }}>8</div>
+        </div>
+      </Tile>
+      <Tile columnCount={2} centerText={false}>
+        <div className="tiles-tile-content-header">Smashing Magazine</div>
+        <div>Smasing Newsletter #177: Sketch On Windows, CSS and Chatbots</div>
+      </Tile>
     </Row>
     <Row>
-      <Tile>1</Tile>
-      <Tile>2</Tile>
-      <Tile>
-        <img src="https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg" alt="cat" />
+      <Tile columnCount={2} centerText={false}>
+        <div>
+          <div className="tiles-tile-center-image">
+            <img src="https://maxcdn.icons8.com/Share/icon/win8/Logos//ms_edge1600.png" alt="edge" />
+          </div>
+        </div>
+        <div className="tiles-tile-align-bottom">Microsoft Edge</div>
       </Tile>
-      <Tile>4</Tile>
+      <Tile padText={false}>
+        <img src="https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg" alt="cat" />
+        <span className="tiles-tile-align-bottom">Bilder</span>
+      </Tile>
+    </Row>
+    <Row>
+      <Tile centerText={false}>
+        <span className="tiles-tile-content-header">-4</span>
+        <span className="tiles-tile-align-bottom">Oslo</span>
+      </Tile>
     </Row>
   </div>;
 
